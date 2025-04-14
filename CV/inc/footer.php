7f9f4a052a58ab250444b0c874d532137306eb30
alexis viallesseche <144714893+alexisvlsch@users.noreply.github.com>
2025-04-14 11:55:22 +0200

    Delete CV directory

--- CV/inc/footer.php
+++
@@ -1,6 +0,0 @@
-<!-- inc/footer.php -->
-<footer>
-  <p>&copy; <?php echo date("Y"); ?> Alexis Viallesseche. Tous droits réservés.</p>
-</footer>
-</body>
-</html>
--- CV/inc/nav.php
+++
@@ -1,9 +0,0 @@
-<!-- inc/nav.php -->
-<nav>
-  <ul>
-    <li><a href="index.php">Accueil</a></li>
-    <li><a href="cv.php">CV</a></li>
-    <li><a href="profile.php">Profil</a></li>
-    <li><a href="gallery.php">Galerie</a></li>
-  </ul>
-</nav>
